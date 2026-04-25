2743f69a559fe64b5d3be9e35d1b145335baf23e
rchapkailo <romafromukraine@gmail.com>
2026-04-25 21:09:27 +0300

    feat!: add shared dep, rename getRepoName to getRepoInfo returning RepoInfo with deps

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
+import { REPO_NAME as SHARED_REPO_NAME } from "@rchapkailo/shared";
+
 export const REPO_NAME = "child-y";
 
-export function getRepoName(): string {
-	return REPO_NAME;
+export interface RepoInfo {
+	name: string;
+	deps: string[];
 }
 
-console.log(`Repo: ${getRepoName()}`);
+export function getRepoInfo(): RepoInfo {
+	return { name: REPO_NAME, deps: [`@rchapkailo/${SHARED_REPO_NAME}`] };
+}
+
+const info = getRepoInfo();
+console.log(`Repo: ${info.name}, deps: ${info.deps.join(", ")}`);
