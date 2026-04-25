861870d3cff4ee1ffdafe101c9d5478190efce87
rchapkailo <romafromukraine@gmail.com>
2026-04-25 22:52:41 +0300

    chore: consolidate release workflow and tooling updates

--- index.ts
+++ index.ts
@@ -3,16 +3,16 @@ import { REPO_NAME as SHARED_REPO_NAME } from "@rchapkailo/shared";
 export const REPO_NAME = "child-y";
 
 export interface RepoInfo {
-	name: string;
-	deps: string[];
+  name: string;
+  deps: string[];
 }
 
 export function getRepoInfo(): RepoInfo {
-	return { name: REPO_NAME, deps: [`@rchapkailo/${SHARED_REPO_NAME}`] };
+  return { name: REPO_NAME, deps: [`@rchapkailo/${SHARED_REPO_NAME}`] };
 }
 
 export function getRepoName(): string {
-	return REPO_NAME;
+  return REPO_NAME;
 }
 
 const info = getRepoInfo();
