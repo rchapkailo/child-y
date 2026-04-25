ceb14de62dfe5463c996f87d276879fe3bc5c59c
rchapkailo <romafromukraine@gmail.com>
2026-04-25 21:35:20 +0300

    fix: expose package declarations

--- index.ts
+++ index.ts
@@ -11,5 +11,9 @@ export function getRepoInfo(): RepoInfo {
 	return { name: REPO_NAME, deps: [`@rchapkailo/${SHARED_REPO_NAME}`] };
 }
 
+export function getRepoName(): string {
+	return REPO_NAME;
+}
+
 const info = getRepoInfo();
 console.log(`Repo: ${info.name}, deps: ${info.deps.join(", ")}`);
